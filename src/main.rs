441a4aa5dd3de5fbaa3d9746aa7982d7eaa1d323
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-09-17 23:00:55 +0200

    files: modularized material

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
 //! Create a custom material to draw basic lines in 3D
 
-use bevy::{
-    input::mouse::MouseMotion,
-    pbr::{MaterialPipeline, MaterialPipelineKey},
-    prelude::*,
-    reflect::{TypePath, TypeUuid},
-    render::{
-        mesh::{MeshVertexBufferLayout, PrimitiveTopology},
-        render_resource::{
-            AsBindGroup, PolygonMode, RenderPipelineDescriptor, ShaderRef,
-            SpecializedMeshPipelineError,
-        },
-    },
-};
+use bevy::{input::mouse::MouseMotion, prelude::*};
+
+mod material;
+use material::{LineList, LineMaterial};
 
 fn main() {
     App::new()
@@ -57,48 +48,6 @@ fn setup(
         transform: Transform::from_xyz(0.5, 0.5, 5.0).looking_at(Vec3::new(0.5, 0.5, 0.0), Vec3::Y),
         ..default()
     });
-}
-
-#[derive(Default, AsBindGroup, TypeUuid, TypePath, Debug, Clone)]
-#[uuid = "050ce6ac-080a-4d8c-b6b5-b5bab7560d8f"]
-struct LineMaterial {
-    #[uniform(0)]
-    color: Color,
-}
-
-impl Material for LineMaterial {
-    fn fragment_shader() -> ShaderRef {
-        "shaders/line_material.wgsl".into()
-    }
-
-    fn specialize(
-        _pipeline: &MaterialPipeline<Self>,
-        descriptor: &mut RenderPipelineDescriptor,
-        _layout: &MeshVertexBufferLayout,
-        _key: MaterialPipelineKey<Self>,
-    ) -> Result<(), SpecializedMeshPipelineError> {
-        // This is the important part to tell bevy to render this material as a line between vertices
-        descriptor.primitive.polygon_mode = PolygonMode::Line;
-        Ok(())
-    }
-}
-
-/// A list of lines with a start and end position
-#[derive(Debug, Clone)]
-pub struct LineList {
-    pub lines: Vec<(Vec3, Vec3)>,
-}
-
-impl From<LineList> for Mesh {
-    fn from(line: LineList) -> Self {
-        // This tells wgpu that the positions are list of lines
-        // where every pair is a start and end point
-        let mut mesh = Mesh::new(PrimitiveTopology::LineList);
-
-        let vertices: Vec<_> = line.lines.into_iter().flat_map(|(a, b)| [a, b]).collect();
-        mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
-        mesh
-    }
 }
 
 fn mouse_move_camera(
