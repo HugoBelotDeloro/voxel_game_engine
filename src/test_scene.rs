a056ca1d5392993c0b6e66ed673a94d93c94432b
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-09-20 16:45:07 +0200

    render: adjust texture

--- src/test_scene.rs
+++ src/test_scene.rs
@@ -93,7 +93,23 @@ fn setup(
         Transform::from_xyz(0., 0., -3.),
     );
 
-    let cube_mesh_handle = meshes.add(Mesh::from(shape::Cube::default()));
+    let mut cube_mesh = Mesh::from(shape::Cube::default());
+    #[rustfmt::skip]
+    cube_mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, vec![
+        // Front
+        [0., 1.], [0.5, 1.], [0.5, 0.5], [0.5, 1.],
+        // Back
+        [0.5, 0.5], [0.5, 0.], [0., 0.], [0., 0.5],
+        // Right
+        [0.5, 0.5], [0.5, 0.], [0., 0.], [0., 0.5],
+        // Left
+        [0.5, 0.5], [0.5, 0.], [0., 0.], [0., 0.5],
+        // Top
+        [1., 0.], [0.5, 0.], [0.5, 0.5], [1., 0.5],
+        // Bottom
+        [1., 0.], [0.5, 0.], [0.5, 0.5], [1., 0.5],
+    ]);
+    let cube_mesh_handle = meshes.add(cube_mesh);
     let cube_texture_handle: Handle<Image> = asset_server.load("textures/stone.png");
     let cube_material_handle = standard_materials.add(StandardMaterial {
         base_color_texture: Some(cube_texture_handle),
