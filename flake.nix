f85e2d9566fd972dbc682304c9d24420095dbc57
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-09-21 12:43:42 +0200

    flake: use crane for devShell

--- flake.nix
+++ flake.nix
@@ -1,37 +1,78 @@
 {
   description = "";
 
-  inputs.utils.url = "github:numtide/flake-utils";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.fenix = {
     url = "github:nix-community/fenix";
     inputs.nixpkgs.follows = "nixpkgs";
   };
+  inputs.crane = {
+    url = "github:ipetkov/crane";
+    inputs = {
+      flake-utils.follows = "flake-utils";
+      nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs, utils, fenix }:
+  outputs = { self, nixpkgs, flake-utils, fenix, crane }:
 
-    utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-          llvmPackages.bintools
-        ];
-        buildInputs = with pkgs; [
-          fenix.packages.x86_64-linux.stable.toolchain
-          udev
-          alsa-lib
-          vulkan-loader
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXi
-        ];
-      in {
-        devShell = pkgs.mkShell {
-          inherit buildInputs nativeBuildInputs;
-          LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${with pkgs; lib.makeLibraryPath [
+
+        craneLib = crane.lib.${system}.overrideToolchain
+          fenix.packages.${system}.stable.toolchain;
+
+        commonArgs = {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+          buildInputs = with pkgs; [
+            udev
+            alsa-lib
             vulkan-loader
-          ]}";
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+          ];
+
+          nativeBuildInputs = with pkgs; [ pkg-config llvmPackages.bintools ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        bevyGame =
+          craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+
+        bevyGameClippy = craneLib.cargoClippy (commonArgs // {
+          inherit cargoArtifacts;
+          cargoClippyExtraArgs = "--all-targets -- --deny-warnings";
+        });
+
+        bevyGameFmt =
+          craneLib.cargoFmt (commonArgs // { inherit cargoArtifacts; });
+
+      in {
+        # checks = {
+        #   inherit bevyGame bevyGameClippy bevyGameFmt;
+        # };
+
+        # packages.default = bevyGame;
+
+        # apps.default = flake-utils.lib.mkApp {
+        #   drv = bevyGame;
+        # };
+
+        devShell = craneLib.devShell {
+          inputsFrom = [ bevyGame ];
+
+          LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${
+              with pkgs;
+              lib.makeLibraryPath [
+                vulkan-loader
+                # udev and alsaLib might be needed someday
+              ]
+            }";
         };
       });
 }
