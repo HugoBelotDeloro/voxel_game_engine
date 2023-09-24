04dd874cc60e35d7c4cdd42f07bfb71ae42e58a0
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-09-24 22:16:15 +0200

    world: remove init from Chunk

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use bevy::prelude::*;
 
-mod world;
 mod debug_text;
 mod materials;
 mod player;
 mod player_controller;
 mod settings;
+mod world;
 
 fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins.set(ImagePlugin::default_nearest()),
-            world::chunk::TestScenePlugin,
+            world::WorldPlugin,
             player::PlayerPlugin,
             settings::SettingsPlugin,
             debug_text::DebugTextPlugin,
--- src/world/chunk.rs
+++ src/world/chunk.rs
@@ -1,20 +1,9 @@
-use crate::materials::{voxel_material::VoxelMaterial, line_material::LineMaterial};
 use bevy::{
     prelude::*,
     render::mesh::{Indices, PrimitiveTopology},
 };
 
-pub struct TestScenePlugin;
-
-impl Plugin for TestScenePlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup)
-            .add_plugins(MaterialPlugin::<VoxelMaterial>::default())
-            .add_plugins(MaterialPlugin::<LineMaterial>::default());
-    }
-}
-
-const CHUNK_SIZE: usize = 8;
+pub const CHUNK_SIZE: usize = 8;
 
 #[derive(Component)]
 pub struct Chunk {
@@ -144,40 +133,4 @@ impl Chunk {
 
         mesh
     }
-}
-
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut voxel_materials: ResMut<Assets<VoxelMaterial>>,
-    mut line_materials: ResMut<Assets<LineMaterial>>,
-    asset_server: ResMut<AssetServer>,
-) {
-    let chunk = Chunk::half_empty();
-    let cube_mesh = chunk.build_mesh();
-    let cube_mesh_handle = meshes.add(cube_mesh);
-    let cube_texture_handle: Handle<Image> = asset_server.load("textures/stone.png");
-    let cube_material_handle = voxel_materials.add(VoxelMaterial {
-        color_texture: Some(cube_texture_handle),
-    });
-
-    commands.spawn(MaterialMeshBundle {
-        material: cube_material_handle,
-        transform: Transform::from_xyz(0., 0., 0.),
-        mesh: cube_mesh_handle,
-        ..default()
-    });
-
-    let line_material = line_materials.add(LineMaterial { color: Color::BLACK });
-    const OFFSET: f32 = CHUNK_SIZE as f32 / 2.;
-    commands.spawn(MaterialMeshBundle {
-        material: line_material,
-        transform: Transform::from_xyz(OFFSET, OFFSET, OFFSET),
-        mesh: meshes.add(Mesh::from(shape::Cube::new(CHUNK_SIZE as f32))),
-        ..default()});
-
-    commands.insert_resource(AmbientLight {
-        brightness: 1.,
-        ..default()
-    });
 }
