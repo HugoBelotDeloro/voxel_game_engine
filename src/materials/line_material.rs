5c12e345acf682f6d22cde388fc03fa5633de4bf
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-09-24 19:03:18 +0200

    render(materials): add LineMaterial

---
+++ src/materials/line_material.rs
@@ -0,0 +1,35 @@
+use bevy::{
+    pbr::{MaterialPipeline, MaterialPipelineKey},
+    prelude::*,
+    reflect::{TypePath, TypeUuid},
+    render::{
+        mesh::MeshVertexBufferLayout,
+        render_resource::{
+            AsBindGroup, PolygonMode, RenderPipelineDescriptor, ShaderRef,
+            SpecializedMeshPipelineError,
+        },
+    },
+};
+
+impl Material for LineMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/line_material.wgsl".into()
+    }
+
+    fn specialize(
+        _pipeline: &MaterialPipeline<Self>,
+        descriptor: &mut RenderPipelineDescriptor,
+        _layout: &MeshVertexBufferLayout,
+        _key: MaterialPipelineKey<Self>,
+    ) -> Result<(), SpecializedMeshPipelineError> {
+        descriptor.primitive.polygon_mode = PolygonMode::Line;
+        Ok(())
+    }
+}
+
+#[derive(TypePath, AsBindGroup, Debug, Clone, TypeUuid)]
+#[uuid = "2a862800-1c7c-4a44-aa2d-a21a11b4e6f4"]
+pub struct LineMaterial {
+    #[uniform(0)]
+    color: Color,
+}
--- src/materials/mod.rs
+++ src/materials/mod.rs
@@ -1,1 +1,2 @@
+pub mod line_material;
 pub mod voxel_material;
