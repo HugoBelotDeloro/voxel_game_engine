defe82a436a89caf0713fff4155a989bc52b2671
Hugo Belot-Deloro <hugo.belot-deloro@epita.fr>
2023-09-28 22:12:39 +0200

    chunk: rendering refactor

--- src/world/chunk/mod.rs
+++ src/world/chunk/mod.rs
@@ -50,76 +50,106 @@ impl Chunk {
         let mut indices = Vec::new();
         let mut current_index = 0u32;
 
-        for i in 0..CHUNK_SIZE {
-            for j in 0..CHUNK_SIZE {
-                for k in 0..CHUNK_SIZE {
-                    let pos = [i, j, k];
-                    if self.get_block(&pos) {
-                        let min_x = (i) as f32;
-                        let max_x = (i + 1) as f32;
-                        let min_y = (j) as f32;
-                        let max_y = (j + 1) as f32;
-                        let min_z = (k) as f32;
-                        let max_z = (k + 1) as f32;
-                        let vertices = &[
-                            // (coordinates, normals, uv mapping)
-                            // Front
-                            ([min_x, min_y, max_z], [0., 0., 1.0], [0., 1.]),
-                            ([max_x, min_y, max_z], [0., 0., 1.0], [0.5, 1.]),
-                            ([max_x, max_y, max_z], [0., 0., 1.0], [0.5, 0.5]),
-                            ([min_x, max_y, max_z], [0., 0., 1.0], [0.5, 1.]),
-                            // Back
-                            ([min_x, max_y, min_z], [0., 0., -1.0], [0.5, 0.]),
-                            ([max_x, max_y, min_z], [0., 0., -1.0], [0., 0.]),
-                            ([max_x, min_y, min_z], [0., 0., -1.0], [0., 0.5]),
-                            ([min_x, min_y, min_z], [0., 0., -1.0], [0.5, 0.5]),
-                            // Right
-                            ([max_x, min_y, min_z], [1.0, 0., 0.], [0.5, 0.5]),
-                            ([max_x, max_y, min_z], [1.0, 0., 0.], [0.5, 0.]),
-                            ([max_x, max_y, max_z], [1.0, 0., 0.], [0., 0.]),
-                            ([max_x, min_y, max_z], [1.0, 0., 0.], [0., 0.5]),
-                            // Left
-                            ([min_x, min_y, max_z], [-1.0, 0., 0.], [0.5, 0.5]),
-                            ([min_x, max_y, max_z], [-1.0, 0., 0.], [0.5, 0.]),
-                            ([min_x, max_y, min_z], [-1.0, 0., 0.], [0., 0.]),
-                            ([min_x, min_y, min_z], [-1.0, 0., 0.], [0., 0.5]),
-                            // Top
-                            ([max_x, max_y, min_z], [0., 1.0, 0.], [1., 0.]),
-                            ([min_x, max_y, min_z], [0., 1.0, 0.], [0.5, 0.]),
-                            ([min_x, max_y, max_z], [0., 1.0, 0.], [0.5, 0.5]),
-                            ([max_x, max_y, max_z], [0., 1.0, 0.], [1., 0.5]),
-                            // Bottom
-                            ([max_x, min_y, max_z], [0., -1.0, 0.], [1., 0.]),
-                            ([min_x, min_y, max_z], [0., -1.0, 0.], [0.5, 0.]),
-                            ([min_x, min_y, min_z], [0., -1.0, 0.], [0.5, 0.5]),
-                            ([max_x, min_y, min_z], [0., -1.0, 0.], [1., 0.5]),
-                        ];
+        for [i, j, k] in Chunk::chunk_coords_iter() {
+            let pos = [i, j, k];
+            if self.get_block(&pos) {
+                let min_x = (i) as f32;
+                let max_x = (i + 1) as f32;
+                let min_y = (j) as f32;
+                let max_y = (j + 1) as f32;
+                let min_z = (k) as f32;
+                let max_z = (k + 1) as f32;
 
-                        let cube_positions: Vec<[f32; 3]> =
-                            vertices.iter().map(|(p, _, _)| *p).collect();
-                        let cube_normals: Vec<_> = vertices.iter().map(|(_, n, _)| *n).collect();
-                        let cube_uvs: Vec<_> = vertices.iter().map(|(_, _, uv)| *uv).collect();
-
-                        positions.extend(cube_positions);
-                        normals.extend(cube_normals);
-                        uvs.extend(cube_uvs);
-                        const LOCAL_INDICES: [u32; 36] = [
-                            0, 1, 2, 2, 3, 0, // front
-                            4, 5, 6, 6, 7, 4, // back
-                            8, 9, 10, 10, 11, 8, // right
-                            12, 13, 14, 14, 15, 12, // left
-                            16, 17, 18, 18, 19, 16, // top
-                            20, 21, 22, 22, 23, 20, // bottom
-                        ];
-                        indices.extend(
-                            LOCAL_INDICES
-                                .iter()
-                                .map(|i| i + current_index)
-                                .collect::<Vec<u32>>(),
-                        );
-                        current_index += 24;
-                    }
+                /// Used for the following code.
+                enum Face {
+                    Front = 0,
+                    Back = 1,
+                    Right = 2,
+                    Left = 3,
+                    Top = 4,
+                    Bottom = 5,
                 }
+
+                const NORMALS: [[f32; 3]; 6] = [
+                    [0., 0., 1.],
+                    [0., 0., -1.],
+                    [1., 0., 0.],
+                    [-1., 0., 0.],
+                    [0., 1., 0.],
+                    [0., -1., 0.],
+                ];
+
+                const UV_MAPPINGS: [[[f32; 2]; 4]; 6] = [
+                    [[0., 1.], [0.5, 1.], [0.5, 0.5], [0.5, 1.]],
+                    [[0.5, 0.], [0., 0.], [0., 0.5], [0.5, 0.5]],
+                    [[0.5, 0.5], [0.5, 0.], [0., 0.], [0., 0.5]],
+                    [[0.5, 0.5], [0.5, 0.], [0., 0.], [0., 0.5]],
+                    [[1., 0.], [0.5, 0.], [0.5, 0.5], [1., 0.5]],
+                    [[1., 0.], [0.5, 0.], [0.5, 0.5], [1., 0.5]],
+                ];
+
+                let vertices_coordinates = [
+                    [
+                        [min_x, min_y, max_z],
+                        [max_x, min_y, max_z],
+                        [max_x, max_y, max_z],
+                        [min_x, max_y, max_z],
+                    ],
+                    [
+                        [min_x, max_y, min_z],
+                        [max_x, max_y, min_z],
+                        [max_x, min_y, min_z],
+                        [min_x, min_y, min_z],
+                    ],
+                    [
+                        [max_x, min_y, min_z],
+                        [max_x, max_y, min_z],
+                        [max_x, max_y, max_z],
+                        [max_x, min_y, max_z],
+                    ],
+                    [
+                        [min_x, min_y, max_z],
+                        [min_x, max_y, max_z],
+                        [min_x, max_y, min_z],
+                        [min_x, min_y, min_z],
+                    ],
+                    [
+                        [max_x, max_y, min_z],
+                        [min_x, max_y, min_z],
+                        [min_x, max_y, max_z],
+                        [max_x, max_y, max_z],
+                    ],
+                    [
+                        [max_x, min_y, max_z],
+                        [min_x, min_y, max_z],
+                        [min_x, min_y, min_z],
+                        [max_x, min_y, min_z],
+                    ],
+                ];
+
+                let cube_positions: Vec<[f32; 3]> =
+                    vertices_coordinates.iter().flat_map(|p| *p).collect();
+                let cube_normals: Vec<_> = NORMALS.iter().flat_map(|n| [*n; 4]).collect();
+                let cube_uvs: Vec<_> = UV_MAPPINGS.iter().flat_map(|uv| *uv).collect();
+
+                positions.extend(cube_positions);
+                normals.extend(cube_normals);
+                uvs.extend(cube_uvs);
+                const LOCAL_INDICES: [u32; 36] = [
+                    0, 1, 2, 2, 3, 0, // front
+                    4, 5, 6, 6, 7, 4, // back
+                    8, 9, 10, 10, 11, 8, // right
+                    12, 13, 14, 14, 15, 12, // left
+                    16, 17, 18, 18, 19, 16, // top
+                    20, 21, 22, 22, 23, 20, // bottom
+                ];
+                indices.extend(
+                    LOCAL_INDICES
+                        .iter()
+                        .map(|i| i + current_index)
+                        .collect::<Vec<u32>>(),
+                );
+                current_index += 24;
             }
         }
 
